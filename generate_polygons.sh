390f783ca7e6980a733ffce49f34e92bcdc914ef
Rik <brik@student.ethz.ch>
2019-04-04 00:03:33 +0200

    Change test set range.

--- generate_polygons.sh
+++ generate_polygons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for (( i=0; i<16; i+=5 )) ; do
+for (( i=0; i<16; i+=1 )) ; do
   yaml_dir="./export/${i}"
   mkdir -p $yaml_dir
   python3 generate_synthetic_data.py -ns 100 -no ${i} -d ./data/ -y $yaml_dir --hull_size 1000 1000 --no_overlaps --scale 0.1
